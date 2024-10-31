d9530bc04be2f6ab5a8031efe239fe98845f8cef
Ex-Ryden <freefireuse1233@gmail.com>
2024-10-31 20:49:20 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "2348149825074"
+global.ownername = "Ex-Ryden"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '2348149825074'  //creator number
+global.ownername = 'Ex-Ryeni' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
